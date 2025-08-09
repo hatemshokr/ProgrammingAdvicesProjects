104ec88d6e91a5a400d25f29668df88562252016
Hatem <hatemshokr@outlook.com>
2025-08-09 22:03:11 +0300

    #12 Solved

---
+++ Introduction to programming using C++/12.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //#1. Print your name, age, city, and country
+    cout << "********************\n";
+    cout << "Name: Hatem Shokr\n";
+    cout << "Age: 20\n";
+    cout << "City: Menofia\n";
+    cout << "Country: Egypt\n";
+    cout << "********************\n" << endl;
+    //#2. Print 4 lines of asterisks
+    cout << "********************\n";
+    cout << "********************\n";
+    cout << "********************\n";
+    cout << "********************\n" << endl;
+    //#3. print some text
+    cout << "I Love Programming!\n";
+    cout << "I promise to be the best developer ever!\n";
+    cout << "I know it will take some time to practice, but I will achieve my goal.\n";
+    cout << "Best Regards,\n";
+    cout << "Hatem Shokr\n";
+    return 0;
+}
